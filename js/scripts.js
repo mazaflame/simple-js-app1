4477515aca47dcbf7cd7b5e9f3a73f3d7fb77282
mazaflame <89023931+mazaflame@users.noreply.github.com>
2022-10-17 17:59:18 -0300

    Started Pokédex project - added 5 pokemons
    added 5 pokemons objects, types and height variables included.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert('Hello world');
+let pokemonList = [
 
-let favoriteFood = 'Pizza'
-document.write(favoriteFood)
+  {
+    name: 'Charmander',
+    type: ["Monster", "Dragon"],
+    height: 0.6,
+  },
+
+  {
+    name: 'Blastoise',
+    type: ["Monster", "Water 1"],
+    height: 1.6,
+  },
+
+  {
+    name: 'Pikachu',
+    type: ["Field", "Fairy"],
+    height: 0.4,
+  },
+
+  {
+    name: 'Snorlax',
+    type: ["Monster"],
+    height: 2.1,
+  },
+
+  {
+    name: 'Psyduck',
+    type: ["Water 1", "Field"],
+    height: 0.8,
+  },
+
+
+
+
+]
