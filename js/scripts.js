a892a29b1c96b2c0cad2975c18b96fb5d180375c
mazaflame <89023931+mazaflame@users.noreply.github.com>
2022-10-31 12:41:42 -0300

    changed loop to forEach
    deleted
    for (let i = 0; i <= pokemonList.length - 1; i++) {
      let pokemon = pokemonList[i]

--- js/scripts.js
+++ js/scripts.js
@@ -32,30 +32,28 @@ let pokemonList = [
 
 ]
 
-// added a loop to display the pokemon's names, heights and special message for each condition,
-//i=0 goest through the whole list, which contains 5 objetcs, therefore the ''-1'', because counting starts at 0, so the count 5 would stop at 4
-// loop using, 'for', 'let pokemon = pokemonlist - to shorten the code below'
-// var text also to shorten the code
-// at the end, using just one document.write connecting it to the var text
 
-for (let i = 0; i <= pokemonList.length - 1; i++) {
-  let pokemon = pokemonList[i]
+pokemonList.forEach(function(pokemon){
+  console.log(pokemon);
+
+
   var text = ""
 
 
   if (pokemon.height >= 1.7) {
-    text = pokemon.name  + " - height: " + pokemon.height + " - WOW! this is a big Pokemon"
+    text = pokemon.name + " - height: " + pokemon.height + " - WOW! this is a big Pokemon"
 
 
   } else if (pokemon.height <= 1.6 && pokemon.height >= 1.0) {
 
-    text = pokemon.name  + " - height: " + pokemon.height + "  - medium size"
+    text = pokemon.name + " - height: " + pokemon.height + "  - medium size"
 
   } else {
 
-    text = pokemon.name  + " - height: " + pokemon.height + "  - small size"
+    text = pokemon.name + " - height: " + pokemon.height + "  - small size"
 
 
   }
   document.write(text + "<br>")
-}
+
+});
