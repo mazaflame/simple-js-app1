36df8d8076eccf3d94616280c23eb63e3304a35e
mazaflame <89023931+mazaflame@users.noreply.github.com>
2022-11-03 18:32:45 -0300

    Cache / event delegation / doc fragment to append loop /  showdetails
    -document fragment which you can append new elements
    
    -showDetails to events from the list listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,73 @@
+
+// Cache the list element + add a listener to it
+const list = document.querySelector('.pokemon-list');
+list.addEventListener('click', showDetails);
+
+const pokemonList = [
+
+  {
+    name: 'Charmander',
+    type: ["Monster", "Dragon"],
+    height: 0.6,
+  },
+
+  {
+    name: 'Blastoise',
+    type: ["Monster", "Water 1"],
+    height: 1.6,
+  },
+
+  {
+    name: 'Pikachu',
+    type: ["Field", "Fairy"],
+    height: 0.4,
+  },
+
+  {
+    name: 'Snorlax',
+    type: ["Monster"],
+    height: 2.1,
+  },
+
+  {
+    name: 'Psyduck',
+    type: ["Water 1", "Field"],
+    height: 0.8,
+  },
+
+]
+
+// Check that the child element that fired
+// the event is a button, and log its text content
+function showDetails(e) {
+  if (event.target.matches('button')) {
+    console.log(event.target.textContent); //e.target
+  }
+}
+
+// Create a document fragment
+const frag = document.createDocumentFragment();
+
+// Loop over the array and for each object
+// add create a list item, and a button,  append the button
+// to the list item, and then the list item to the fragment
+pokemonList.forEach(pokemon => {
+  const listItem = document.createElement('li');
+  const button = document.createElement('button');
+  button.textContent = pokemon.name;
+  button.classList.add('stylez');
+  listItem.appendChild(button);
+  frag.appendChild(listItem);
+});
+
+// append the fragment of list items
+// to the cached list element
+list.appendChild(frag);
+
+
+
+/*  ***** OLD CODE, switched to the one above
+
 let pokemonRepository = (function() {
   let pokemonList = [];
 
@@ -63,9 +133,13 @@ function eventListener(button, pokemon) {
 
 }
 
-function showDetails(pokemon) {
-  console.log(pokemon.name);
-}
+ let showDetails = document.querySelector(".pokemon-list");
+
+showDetails("button")addEventListener( "click", showDetails));
+
+
+
+
 
 pokemonList.forEach(function(pokemon) {
   let pokemonList = document.querySelector(".pokemon-list");
@@ -79,4 +153,4 @@ pokemonList.forEach(function(pokemon) {
 
 
 
-});
+});  */
