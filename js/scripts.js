60e7ee180a37cc4d957f552697b7be4959a00580
mazaflame <89023931+mazaflame@users.noreply.github.com>
2022-11-03 19:20:32 -0300

    fixes: showDetails properly assigned
    showDetails assigned to the object as a whole, not just the name

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,83 @@
+let pokemonRepository = (function() {
+  let pokemonList = [];
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})(); //IIFE to safely lock everything into place
+
+let pokemonList = [
+
+  {
+    name: 'Charmander',
+    type: ["Monster", "Dragon"],
+    height: 0.6,
+  },
+
+  {
+    name: 'Blastoise',
+    type: ["Monster", "Water 1"],
+    height: 1.6,
+  },
+
+  {
+    name: 'Pikachu',
+    type: ["Field", "Fairy"],
+    height: 0.4,
+  },
+
+  {
+    name: 'Snorlax',
+    type: ["Monster"],
+    height: 2.1,
+  },
+
+  {
+    name: 'Psyduck',
+    type: ["Water 1", "Field"],
+    height: 0.8,
+  },
+
+]
+
+
+function showDetails(pokemon){
+  console.log(pokemon)
+}
+
+
+
+
+
+
+pokemonList.forEach((pokemon) => {
+  let pokemonList = document.querySelector(".pokemon-list");
+  let listItem = document.createElement("li");
+  let button = document.createElement("button");
+  button.innerText = pokemon.name;
+  button.classList.add("stylez");
+  button.addEventListener("click", ()=>showDetails(pokemon));
+  listItem.appendChild(button);
+  pokemonList.appendChild(listItem);
+
+
+
+
+});
+
+
+
+
+/*  ***** OLD CODE, switched to the one above
 // Cache the list element + add a listener to it
 const list = document.querySelector('.pokemon-list');
 list.addEventListener('click', showDetails);
@@ -66,91 +145,4 @@ list.appendChild(frag);
 
 
 
-/*  ***** OLD CODE, switched to the one above
-
-let pokemonRepository = (function() {
-  let pokemonList = [];
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  return {
-    add: add,
-    getAll: getAll
-  };
-})(); //IIFE to safely lock everything into place
-
-let pokemonList = [
-
-  {
-    name: 'Charmander',
-    type: ["Monster", "Dragon"],
-    height: 0.6,
-  },
-
-  {
-    name: 'Blastoise',
-    type: ["Monster", "Water 1"],
-    height: 1.6,
-  },
-
-  {
-    name: 'Pikachu',
-    type: ["Field", "Fairy"],
-    height: 0.4,
-  },
-
-  {
-    name: 'Snorlax',
-    type: ["Monster"],
-    height: 2.1,
-  },
-
-  {
-    name: 'Psyduck',
-    type: ["Water 1", "Field"],
-    height: 0.8,
-  },
-
-]
-
-// I tried the code below first:
-//let addPokemonEventListener = function(element, pokemon) {
-//  element.addEventListener("click", () => showDetails(pokemon));
-//};
-
-//then I started the one below
-
-function eventListener(button, pokemon) {
-  button.addEventListener("click", function() {
-    showDetails(pokemon);
-  });
-
-}
-
- let showDetails = document.querySelector(".pokemon-list");
-
-showDetails("button")addEventListener( "click", showDetails));
-
-
-
-
-
-pokemonList.forEach(function(pokemon) {
-  let pokemonList = document.querySelector(".pokemon-list");
-  let listItem = document.createElement("li");
-  let button = document.createElement("button");
-  button.innerText = pokemon.name;
-  button.classList.add("stylez");
-  listItem.appendChild(button);
-  pokemonList.appendChild(listItem);
-
-
-
-
-});  */
+*/
