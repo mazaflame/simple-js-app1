812c42fe99f77d92056583d12a1f8fe106eabe1a
mazaflame <89023931+mazaflame@users.noreply.github.com>
2022-11-10 16:28:36 -0300

    tried to implement MODAL
    failed

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,92 @@
-  let pokemonRepository = (function() {
-    let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
+  let modalContainer = document.querySelector('#modal-container');
 
-    function add(pokemon) {
-      if (
-        typeof pokemon === "object" &&
-        "name" in pokemon
-      ) {
-        pokemonList.push(pokemon);
-      } else {
-        console.log("pokemon is not correct");
-      }
+  // REST OF CODE
+  function showModal(title, text, img) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+//Add
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    //add content carai
+    let closeBtn = document.createElement('button');
+    closeBtnElement.classList.add('close-Btn');
+    closeBtnElement.innerText.add('Close');
+    closeBtnElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal(){
+modalContainer.classList.remove('is-visible');
+
+  }
+  
+  window.addEventListener('keydwon', (e)=>{
+    if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+      hideModal();
     }
-    function getAll() {
-      return pokemonList;
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+  
+    // We only want to close if the user clicks directly outside
+    let target = e.target; // ????
+    if (target === modalContainer) {
+      hideModal();
     }
+  });
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return pokemonList;
+  }
 
 
-
-  function addListItem(pokemon)  {
+  function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("stylez");
+    //button.setAttribute('data-toggle', 'modal');
+    //button.setAttribute('data-target', 'pokemon-modal')
+    //  button.classList.add('btn');
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
-    button.addEventListener("click", function(event) {
-      showDetails(pokemon); 
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);  //event handler
 
 
 
 
-  });
+    });
   }
 
-    
+  /////LOAD THE LIST
+
   function loadList() {
-    return fetch(apiUrl) .then(function (response) {
+    return fetch(apiUrl).then(function (response) {
       return response.json();
     }).then(function (json) {
       json.results.forEach(function (item) {
@@ -70,99 +119,35 @@
     });
   }
 
-  function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-      console.log(item);
-    });
-    }
 
+  
 
-
-
-    return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem,
-      loadList: loadList,
-      loadDetails: loadDetails,
-      showDetails: showDetails
+function showDetails(item) {
+   // pokemonRepository.loadDetails(item).then(function () {
+      // console.log(item);
+      showModal(pokemon.name, item.imageUrl, item.height, item.types)
     };
-  })();
+  
 
-  pokemonRepository.loadList().then(function () {
-    pokemonRepository.getAll().forEach(function (pokemon) {
-      pokemonRepository.addListItem(pokemon);
-    });
+
+
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
+})();
+
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
   });
+});
 
 
 
-  /*  ***** OLD CODE, switched to the one above
-  // Cache the list element + add a listener to it
-  const list = document.querySelector('.pokemon-list');
-  list.addEventListener('click', showDetails);
 
-  const pokemonList = [
-
-    {
-      name: 'Charmander',
-      type: ["Monster", "Dragon"],
-      height: 0.6,
-    },
-
-    {
-      name: 'Blastoise',
-      type: ["Monster", "Water 1"],
-      height: 1.6,
-    },
-
-    {
-      name: 'Pikachu',
-      type: ["Field", "Fairy"],
-      height: 0.4,
-    },
-
-    {
-      name: 'Snorlax',
-      type: ["Monster"],
-      height: 2.1,
-    },
-
-    {
-      name: 'Psyduck',
-      type: ["Water 1", "Field"],
-      height: 0.8,
-    },
-
-  ]
-
-  // Check that the child element that fired
-  // the event is a button, and log its text content
-  function showDetails(e) {
-    if (event.target.matches('button')) {
-      console.log(event.target.textContent); //e.target
-    }
-  }
-
-  // Create a document fragment
-  const frag = document.createDocumentFragment();
-
-  // Loop over the array and for each object
-  // add create a list item, and a button,  append the button
-  // to the list item, and then the list item to the fragment
-  pokemonList.forEach(pokemon => {
-    const listItem = document.createElement('li');
-    const button = document.createElement('button');
-    button.textContent = pokemon.name;
-    button.classList.add('stylez');
-    listItem.appendChild(button);
-    frag.appendChild(listItem);
-  });
-
-  // append the fragment of list items
-  // to the cached list element
-  list.appendChild(frag);
-
-
-
-  *//////
